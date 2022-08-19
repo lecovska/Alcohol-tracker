6f320e637e28d7e10d4871cf9a77ac37c6130866
lecovska <lecovska@gmail.com>
2022-08-19 15:40:16 +0200

    editing css

--- src/App.js
+++ src/App.js
@@ -8,49 +8,49 @@ function App() {
 
   const [percent, setPercent] = useState("");
 
-  const [weight, setWeight]= useState("");
+  const [weight, setWeight] = useState("");
 
   function changeHandler(event) {
     setAmount(event.target.value);
   }
 
-  function percents(event){
+  function percents(event) {
     setPercent(event.target.value);
   }
 
-  function weights(event){
+  function weights(event) {
     setWeight(event.target.value);
   }
 
-const res = ()=>(amount * percent * 0.75 / weight).toFixed(2);
+  const res = () => (amount * percent * 0.75 / weight).toFixed(2);
 
-let status=(amount * percent * 0.75 / weight).toFixed(2);
+  let status = (amount * percent * 0.75 / weight).toFixed(2);
 
-function message(){
-  
-  if(status>0.3){
-    return "Put nastavi taksijem, autobusom ili peske"
-  }else if(status<0.3){
-    return "Mozes da vozis "
+  function message() {
+
+    if (status > 0.3) {
+      return "Put nastavi taksijem, autobusom ili peske"
+    } else if (status < 0.3) {
+      return "Mozes da vozis "
+    }
   }
-}
 
 
   return (
     <div className="app">
       <div className='content'>
         <h2 className='naslov'>ALCOHOL TRACKER</h2>
-      <p>KOLIČINA</p>
-      <input type="number" value={amount} onChange={changeHandler} placeholder="Ukupno popijenih casa"></input>
-      <p>PROCENAT ALKOHOLA</p>
-      <input type="number" value={percent} onChange={percents}placeholder="%"></input>
-      <p>TEŽINA</p>
-      <input type="number" value={weight} onChange={weights} placeholder="kg"></input>
-      <div className='displayRes'>{res()}<span>&#8240;</span></div>
-      <button className="button-30" onClick={res()}>Izračunaj</button>
-      <div className='message'>{message()}</div>
+        <p>KOLIČINA</p>
+        <input type="number" value={amount} onChange={changeHandler} placeholder="Ukupno popijenih casa"></input>
+        <p>PROCENAT ALKOHOLA</p>
+        <input type="number" value={percent} onChange={percents} placeholder="%"></input>
+        <p>TEŽINA</p>
+        <input type="number" value={weight} onChange={weights} placeholder="kg"></input>
+        <div className='displayRes'>{res()}<span>&#8240;</span></div>
+        <button className="button-30" onClick={res()}>Izračunaj</button>
+        {status > 0.3 ? <div id="message2">{message()}</div> : <div className='message'>{message()}</div>}
       </div>
-    </div>
+      </div>
   );
 }
 
